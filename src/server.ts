19da475289118c030ebb3de364bc05e03f990d6f
MD Omar Faruk <muhammadabuhafz@gmail.com>
2026-09-01 08:40:34 +0600

    added console

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,5 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
