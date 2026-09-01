315ed42250380c04aa215e4200e4213326793cab
MD Omar Faruk <muhammadabuhafz@gmail.com>
2026-09-01 11:37:07 +0600

    fix: remove duplicate console log statements in server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,6 @@ app.get("/", (_req: Request, res: Response) => {
 	res.send("Hello World!");
 });
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`); 
+	console.log(`Example app listening on port ${port}`);
 });
